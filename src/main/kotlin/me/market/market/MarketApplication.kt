a010b933c75b5746acdabf0a937c752a4e4b1fad
wanniDev <wanniDev0928@gmail.com>
2023-01-31 14:11:52 +0900

    project init

---
+++ src/main/kotlin/me/market/market/MarketApplication.kt
@@ -0,0 +1,11 @@
+package me.market.market
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarketApplication>(*args)
+}
