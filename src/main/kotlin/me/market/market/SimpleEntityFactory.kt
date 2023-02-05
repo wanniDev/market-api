7c2cc88e7e9811adf2af412f313c6dfb0d56882e
wanniDev <wanniDev0928@gmail.com>
2023-02-05 11:55:33 +0900

    Feat: 리팩토링 및 디버깅 중에 발생할 수 있는 dto와 entity 필드 수정 시 발생하는 수정범위를 최대한 줄이기 위해 simpleFactory 패턴 적용

---
+++ src/main/kotlin/me/market/market/SimpleEntityFactory.kt
@@ -0,0 +1,18 @@
+package me.market.market
+
+import me.market.market.member.api.request.MemberRegisterReq
+import me.market.market.member.domain.AccountInfo
+import me.market.market.member.domain.Member
+import me.market.market.member.domain.PhoneInfo
+
+class SimpleEntityFactory {
+    companion object {
+        fun createMemberFrom(memberRegisterReq: MemberRegisterReq): Member {
+            return Member(
+                AccountInfo(memberRegisterReq.accountBank, memberRegisterReq.accountNum),
+                PhoneInfo(memberRegisterReq.mobileCarrier, memberRegisterReq.phoneNum),
+                memberRegisterReq.passwd
+            )
+        }
+    }
+}
