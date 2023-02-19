952442a60bd1db02e58ef3e6c35e671615deb25a
wanniDev <wanniDev0928@gmail.com>
2023-02-20 00:23:04 +0900

    Feat: gradle querydsl 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,12 @@ plugins {
     id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
+
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
+    kotlin("kapt") version "1.7.22"
+    idea
 }
 
 group = "me.spring"
@@ -14,12 +17,14 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
+    maven("https://plugins.gradle.org/m2/")
     mavenCentral()
 }
 
 val asciidoctorExtensions: Configuration by configurations.creating
 
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -41,6 +46,19 @@ dependencies {
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
     asciidoctorExtensions("org.springframework.restdocs:spring-restdocs-asciidoctor")
+
+    implementation("com.querydsl:querydsl-jpa")
+    implementation("com.querydsl:querydsl-core")
+    implementation("com.querydsl:querydsl-apt:5.0.0")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+}
+
+idea {
+    module {
+        val kaptMain = file("build/generated/source/kapt/main")
+        sourceDirs.add(kaptMain)
+        generatedSourceDirs.add(kaptMain)
+    }
 }
 
 val snippetsDir by extra { file("build/generated-snippets") }
